7d270ef982d9fe37e1c6aca952d38b742ff3a838
混的人 <2223544759@qq.com>
2026-02-13 17:13:13 +0800

    add cs61a

---
+++ course/cs61a/hw/hw07/hw07.scm
@@ -0,0 +1,23 @@
+(define (square n) (* n n))
+
+(define (pow base exp)
+    (cond
+        ((zero? exp) base)
+        ((= exp 1) base)
+        ((even? exp) (pow (square base) (/ exp 2)))
+        (else (* base (pow base (- exp 1))))
+    )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let 
+        ((y (repeatedly-cube (- n 1) x)))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
